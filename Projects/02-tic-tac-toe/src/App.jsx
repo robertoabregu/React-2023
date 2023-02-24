e34753fdecde2adcae85fc04e66e56e0543d5b22
robertoabregu <raabregu@gmail.com>
2023-02-24 12:54:56 -0300

    singlequotes errors fixed

--- Projects/02-tic-tac-toe/src/App.jsx
+++ Projects/02-tic-tac-toe/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import confetti from 'canvas-confetti'
-import { Square } from "./components/Square.jsx"
-import { TURNS } from "./constant.js"
-import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constant.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
 import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
-function App() {  
+// eslint-disable-next-line space-before-function-paren
+function App() {
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
     if (boardFromStorage) return JSON.parse(boardFromStorage)
     return Array(9).fill(null)
   })
-    
+
   const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
@@ -32,12 +33,12 @@ function App() {
   const updateBoard = (index) => {
     // No actualiza la posición si ya tiene algo
     if (board[index] || winner) return
-    // Actualiza la posición 
-    const newBoard = [... board]
+    // Actualiza la posición
+    const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
     // Cambiar el turno
-    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     // Guardar la partida
     saveGameToStorage({
@@ -58,7 +59,7 @@ function App() {
     <main className='board'>
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className="game">
+      <section className='game'>
         {
           board.map((square, index) => {
             return (
@@ -66,7 +67,7 @@ function App() {
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
-              > 
+              >
                 {square}
               </Square>
             )
@@ -75,10 +76,10 @@ function App() {
       </section>
 
       <section className='turn'>
-        <Square isSelected={turn == TURNS.X}>
+        <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
         </Square>
-        <Square isSelected={turn == TURNS.O}>
+        <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
       </section>
