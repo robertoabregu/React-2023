52ea55116bd7a058dac2dc05d61c0f48ec2db5da
Roberto Abregú <raabregu@gmail.com>
2023-02-24 15:03:46 -0300

    import removed

--- Projects/02-tic-tac-toe/src/App.jsx
+++ Projects/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useDeferredValue, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import confetti from 'canvas-confetti'
 import { Square } from './components/Square.jsx'
